4af7e1ec18878def320b94d206628ffdc89a3019
Rubén González <rgonzalez@armadortech.com>
2018-04-06 14:23:14 +0200

    Creando la estructura de los principales metodos

--- js/jquery.drag-drop-nested-lists.plugin.js
+++ js/jquery.drag-drop-nested-lists.plugin.js
@@ -1,7 +1,8 @@
 (function($) {
     var defaultOptions = {
         clone: true,
-        acceptFrom: {"list", "list-2"},
+        dragFrom: ["tested-list", "another-list"],
+        dropTo: ["tested-list", "another-list"]
     };
 
 
@@ -11,13 +12,50 @@
     var methods = {
         init: function(options) {
             options = $.extend({}, defaultOptions, options);
+
+            this.find('.item')
+                .draggable()
+                .on( "dragstart", methods.dragStart )
+                .on( "drag", methods.drag )
+                .on( "dragstop", methods.dragStop );
+
+                /*
+                .droppable()
+                .on( "dropover", methods.dropOver )
+                .on( "drop", methods.drop )
+                .on( "dropout", methods.dropOut );
+                */                
+        },
+        
+
+        // Drag events
+
+        dragStart: function(event, ui) {
+            console.log("inicia el drag");
         },
 
-        onDrag: function(event) {},
+        drag: function(event, ui) {
+            console.log("durante el drag");
+        },
 
-        onMove: function(event) {},
+        dragStop: function(event, ui) {
+            console.log("termina el drag");
+        },
 
-        onDrop: function(event) {}
+
+        // Drop events
+
+        dropOver: function(event, ui) {
+            console.log("drop sobre");
+        },
+
+        drop: function(event, ui) {
+            console.log("drop");
+        },
+
+        dropOut: function(event, ui) {
+            console.log("drop out");
+        }
     };
 
 
